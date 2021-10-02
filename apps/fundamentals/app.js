5f27aef5655aff60bd8a824a5c7a6359ffd9ae03
Alain D'Ettorre <alain.det@gmail.com>
2021-10-02 08:28:59 +0200

    Add basics for v-on and v-bind

--- apps/fundamentals/app.js
+++ apps/fundamentals/app.js
@@ -3,12 +3,18 @@ const vm = Vue.createApp({
     return {
       firstName: 'Alain',
       lastName: 'D\'Ettorre',
+      googleUrl: 'https://google.com',
+      rawGoogleUrl: 'Raw HTML <a href="https://google.com" target="_blank">&rarr; Google</a>',
+      counter: 0,
     };
   },
   methods: {
     // Cannot use arrow function since this and getters are used
     fullName() {
       return `${this.firstName} ${this.lastName}`;
+    },
+    incrementCounter() {
+      this.counter += 1;
     }
   },
 }).mount('#app');
