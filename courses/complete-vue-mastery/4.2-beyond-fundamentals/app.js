f15638a8a0d78b687e9a06187cd05be5e0c3cb8e
Alain D'Ettorre <alain.det@gmail.com>
2021-10-05 07:00:18 +0200

    Complete lifecycle hooks example

--- courses/complete-vue-mastery/4.2-beyond-fundamentals/app.js
+++ courses/complete-vue-mastery/4.2-beyond-fundamentals/app.js
@@ -2,7 +2,13 @@ const App = {
 
   data() {
     return {
-      message: 'Hello world!',
+      message: 'The time is: ' + (new Date()).toISOString(),
+    }
+  },
+
+  methods: {
+    updateTime() {
+      this.message = 'The time is: ' + (new Date()).toISOString();
     }
   },
 
@@ -10,6 +16,9 @@ const App = {
     console.log('LIFECYCLE HOOK: beforeCreate', this.message);
   },
 
+  // Creation: Vue instance creation
+  // Use it to access data
+  // ~ Angular's ngOnInit
   created() {
     console.log('LIFECYCLE HOOK: created', this.message);
   },
@@ -18,9 +27,32 @@ const App = {
     console.log('LIFECYCLE HOOK: beforeMount', this.$el);
   },
 
+  // Mounting: compiled template, mounted and rendered into the DOM
+  // Use it to alter the rendered template
+  // ~ Angular's ngAfterViewInit
   mounted() {
     console.log('LIFECYCLE HOOK: mounted', this.$el);
-  }
+  },
+
+  // Triggered before updating any data from the "data" instance property
+  // Cannot discriminate what property changed! :(
+  beforeUpdate() {
+    console.log('LIFECYCLE HOOK: beforeUpdate');
+  },
+
+  // ~ Angular's ngOnChanges
+  updated() {
+    console.log('LIFECYCLE HOOK: updated');
+  },
+
+  beforeUnmount() {
+    console.log('LIFECYCLE HOOK: beforeUnmount');
+  },
+
+  // ~ Angular's ngOnDestroy
+  unmounted() {
+    console.log('LIFECYCLE HOOK: unmounted');
+  },
 
 };
 
