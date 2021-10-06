b405f678eec6e8d1d1b1a13b172476e9b9cafc46
Alain D'Ettorre <alain.det@gmail.com>
2021-10-06 07:10:22 +0200

    Create first Single File Component (SFC)

--- courses/complete-vue-mastery/6-components/src/main.js
+++ courses/complete-vue-mastery/6-components/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+// import Greeting from './components/Greeting.vue';
+
+const vm = createApp(App);
+
+// Global import
+// vm.component('Greeting', Greeting);
+
+vm.mount('#app');
