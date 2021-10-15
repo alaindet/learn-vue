fc4c88e37abffa87eb96099a09034174328c91ea
Alain D'Ettorre <alain.det@gmail.com>
2021-10-15 17:50:56 +0200

    Add scoped slots example

---
+++ courses/complete-vue-mastery/sections/10-scoped-slots/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <hello-world>
+
+      <!--
+      <template v-slot="slotProps">
+        <p>Hello {{ slotProps.user.name }}!</p>
+      </template>
+      -->
+
+      <!--
+      <template #default="slotProps">
+        <p>Hello {{ slotProps.user.name }}</p>
+      </template>
+      -->
+
+      <!--
+      <template #default="{ user }">
+        <p>Hello {{ user.name }}</p>
+      </template>
+      -->
+
+      <!-- Default slot -->
+      <template #default="{ user }">
+        <p>Hello {{ user.name }}</p>
+      </template>
+
+      <!-- Named slot: favorites -->
+      <template #favorites="{ favorites }">
+        <ul>
+          <li v-for="fav in favorites" :key="fav">
+            {{ fav }}
+          </li>
+        </ul>
+      </template>
+
+    </hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ courses/complete-vue-mastery/sections/10-scoped-slots/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <slot :user="user"></slot>
+    <slot name="favorites" :favorites="favorites"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      user: {
+        name: 'John',
+        age: 25,
+      },
+      favorites: [
+        'Pizza',
+        'Marbles',
+        'Birds',
+      ],
+    };
+  },
+}
+</script>
