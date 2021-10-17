a03292de1c5a8e5bffed1b5ab4245d8054eae338
Alain D'Ettorre <alain.det@gmail.com>
2021-10-17 08:11:43 +0200

    Add custom validation

--- courses/complete-vue-mastery/apps/music-player/src/plugins/vee-validate.js
+++ courses/complete-vue-mastery/apps/music-player/src/plugins/vee-validate.js
@@ -3,6 +3,7 @@ import {
   Field as VeeValidateField,
   ErrorMessage as VeeValidateErrorMessage,
   defineRule,
+  configure,
 } from 'vee-validate';
 
 import {
@@ -24,13 +25,46 @@ export default {
     app.component('VeeValidateErrorMessage', VeeValidateErrorMessage);
 
     defineRule('required', required);
+    defineRule('terms_of_service', required); // Custom alias for custom messages
     defineRule('min', min);
     defineRule('max', max);
     defineRule('alpha_spaces', alphaSpaces);
     defineRule('email', email);
     defineRule('min_value', minValue);
     defineRule('max_value', maxValue);
-    defineRule('confirmed', confirmed);
+    defineRule('passwords_mismatch', confirmed);
     defineRule('excluded', excluded);
+    defineRule('country_excluded', excluded); // Custom alias for custom messages
+
+    configure({
+
+      // This gets executed when an error message is needed
+      generateMessage: (context) => {
+        const { field, rule } = context;
+
+        const messages = {
+          required: `The field ${field} is required.`,
+          min: `The field ${field} is too short`,
+          max: `The field ${field} is too long`,
+          alpha_spaces: `The field ${field} may only contain alphabetic characters and spaces.`,
+          email: `The field ${field} must be a valid email.`,
+          min_value: `The field ${field} value is too low`,
+          max_value: `The field ${field} value is too high`,
+          excluded: `You are not allowed to use this value for the field ${field}.`,
+          country_excluded: 'Due to restrictions, we do not accept users from this location.',
+          passwords_mismatch: 'The passwords don\'t match.',
+          terms_of_service: 'You must accept the terms of service.',
+        };
+
+        const message = messages[rule.name] ?? `The field ${field} is invalid.`;
+
+        return message;
+      },
+
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true,
+    });
   },
 };
---
+++ notes/validation.md
@@ -0,0 +1,10 @@
+# Validation with VeeValidate
+
+By default, vee-validate triggers validators at these times
+- After *input* native event
+- After *change* native event
+- After *blur* native event
+- Before *submit* native event
+- If *v-model* is applied, after the model changed
+
+Triggers can be enabled or disabled via configuration
