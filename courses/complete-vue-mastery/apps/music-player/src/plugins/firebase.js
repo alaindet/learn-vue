0598c9225ed12122850a5cfd92a91b7558328f36
Alain D'Ettorre <alain.det@gmail.com>
2021-10-20 20:45:59 +0200

    Move Firebase credentials to .env file

--- courses/complete-vue-mastery/apps/music-player/src/plugins/firebase.js
+++ courses/complete-vue-mastery/apps/music-player/src/plugins/firebase.js
@@ -2,13 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-// TODO: Move to .env?
 const firebaseConfig = {
-  apiKey: 'AIzaSyCmEEQAu5EZWVNeZAa23fZZYYBSNEqRHjE',
-  authDomain: 'music-player-f4ea3.firebaseapp.com',
-  projectId: 'music-player-f4ea3',
-  storageBucket: 'music-player-f4ea3.appspot.com',
-  appId: '1:680248712197:web:8b7baa221fe18b9f40106c',
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
