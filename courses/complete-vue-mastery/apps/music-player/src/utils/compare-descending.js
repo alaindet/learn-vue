319e8d2176ad92a2ccd059a3f6ed34f727caffa1
Alain D'Ettorre <alain.det@gmail.com>
2021-10-25 07:52:33 +0200

    Fix comparing utils

--- courses/complete-vue-mastery/apps/music-player/src/utils/compare-descending.js
+++ courses/complete-vue-mastery/apps/music-player/src/utils/compare-descending.js
@@ -1,12 +1,10 @@
-export default (key) => {
-  return (a, b) => {
-    const aValue = a[key];
-    const bValue = b[key];
+export default (key) => (a, b) => {
+  const aValue = a[key];
+  const bValue = b[key];
 
-    if (aValue === bValue) {
-      return 0;
-    }
+  if (aValue === bValue) {
+    return 0;
+  }
 
-    return aValue > bValue ? -1 : 1;
-  };
+  return aValue > bValue ? -1 : 1;
 };
