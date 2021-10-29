d3847c41b5a0ddba347e03ed68a0965e588caa88
Alain D'Ettorre <alain.det@gmail.com>
2021-10-29 13:37:32 +0200

    Fix vuex module lazy loading

--- courses/complete-vue-mastery/apps/music-player/src/views/About.vue
+++ courses/complete-vue-mastery/apps/music-player/src/views/About.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <h2>{{ foo }}</h2>
+    <p>
+      This page imports a Vuex module with lazy-loading and
+      maps its state to a computed property.
+    </p>
+    <code>state.foo = {{ foo }}</code>
   </div>
 </template>
 
@@ -13,7 +17,7 @@ import dummyModule from '@/store/modules/dummy.lazy';
 export default {
   created() {
     const store = this.$store;
-    if (!(store && store.state && store.state['dummy'])) {
+    if (!(store && store.state && store.state.dummy)) {
       store.registerModule('dummy', dummyModule);
     }
   },
