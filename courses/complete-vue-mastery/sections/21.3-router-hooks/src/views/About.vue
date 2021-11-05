0cdb76a894d76e89fed393a3fd975b232a35dec2
Alain D'Ettorre <alain.det@gmail.com>
2021-11-05 14:46:44 +0100

    Add basic hooks for Vue router

--- courses/complete-vue-mastery/sections/21.3-router-hooks/src/views/About.vue
+++ courses/complete-vue-mastery/sections/21.3-router-hooks/src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>User is {{ $route.params.user }}</h2>
   </div>
 </template>
+
+<script>
+import { ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+export default {
+  setup() {
+    const user = ref(null);
+    const route = useRoute();
+    const router = useRouter();
+    user.value = route.params.user ?? null;
+
+    // Add a hash fragment to the current route
+    router.push({ hash: '#test' });
+  },
+};
+</script>
