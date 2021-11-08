9660ff674a1afcaaa22fb696074357adfa3588f7
Alain D'Ettorre <alain.det@gmail.com>
2021-11-08 08:13:34 +0100

    Add fake jobs to Hyrule Jobs app

--- courses/net-ninja-vue-typescript/hyrule-jobs/src/App.vue
+++ courses/net-ninja-vue-typescript/hyrule-jobs/src/App.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="app">
-    Hello, Hyrule Jobs!
+    <jobs-list :jobs="jobs" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+
+import { Job } from '@/types';
+import { INITIAL_JOBS } from '@/consts';
+import JobsList from '@/components/JobsList.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: {
+    JobsList,
+  },
+  setup() {
+    const jobs = ref<Job[]>(INITIAL_JOBS);
+
+    return {
+      jobs,
+    };
+  },
 });
 </script>
---
+++ courses/net-ninja-vue-typescript/hyrule-jobs/src/components/JobsList.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Jobs List</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  // setup() {
+
+  // },
+})
+</script>
+
+<style scoped>
+
+</style>
