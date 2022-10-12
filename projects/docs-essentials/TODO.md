92da15334547ca44436715c6ca140515f7cb1a37
Alain D'Ettorre <alain.det@gmail.com>
2022-10-13 01:17:29 +0200

    WIP

---
+++ projects/docs-essentials/TODO.md
@@ -0,0 +1,1 @@
+https://vuejs.org/guide/essentials/computed.html
---
+++ projects/docs-essentials/src/components/MyCounter.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref, nextTick } from 'vue';
+
+const counter = ref(0);
+
+function increment() {
+  counter.value++;
+  nextTick(() => console.log('this happens after updating the DOM'));
+}
+</script>
+
+<template>
+  <button @click="increment">
+    {{ counter }}
+  </button>
+</template>
